683ff3d90678e66fcff7b06e5c8607647091d75d
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-09 09:15:04 -0300

    Refactor Item POJO

--- src/main/java/com/edson/app/item/models/Item.java
+++ src/main/java/com/edson/app/item/models/Item.java
@@ -13,6 +13,22 @@ public class Item {
 		this.quantidade = quantidade;
 	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
 	public Double getTotal() {
 		return produto.getPreco() * quantidade;
 	}
