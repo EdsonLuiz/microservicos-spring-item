25634395154143aa37551f9d446e53c12967bdf4
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-09 09:57:22 -0300

    Add item interface with for feign

---
+++ src/main/java/com/edson/app/item/models/services/ItemServiceFeign.java
@@ -0,0 +1,37 @@
+package com.edson.app.item.models.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.edson.app.item.clientes.ProdutoClienteRest;
+import com.edson.app.item.models.Item;
+import com.edson.app.item.models.Produto;
+
+@Service("serviceFeign")
+@Primary
+public class ItemServiceFeign implements ItemService {
+
+	@Autowired
+	private ProdutoClienteRest produtoClienteRestFeign;
+	
+	@Override
+	public List<Item> findAll() {
+		return produtoClienteRestFeign
+				.findAll()
+				.stream()
+				.map(p -> new Item(p, 1))
+				.collect(Collectors.toList())
+				;
+	}
+
+	@Override
+	public Item findById(Long id, Integer quantidade) {
+		Produto produto = produtoClienteRestFeign.findById(id);
+		return new Item(produto, quantidade);
+	}
+
+}
