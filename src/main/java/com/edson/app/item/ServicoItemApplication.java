ba8aafa8b6a5308a74750892551819fe3851a5ae
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-09 08:08:59 -0300

    Start project

---
+++ src/main/java/com/edson/app/item/ServicoItemApplication.java
@@ -0,0 +1,13 @@
+package com.edson.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edson/app/item/ServicoItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.app.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
