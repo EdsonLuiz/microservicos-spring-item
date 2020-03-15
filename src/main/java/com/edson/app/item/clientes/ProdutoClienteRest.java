bb72601c6a6a6bd93988ec051d6aa83ce0b4fdcd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-15 11:19:10 -0300

    Add CRUD with RestTemplate

--- src/main/java/com/edson/app/item/clientes/ProdutoClienteRest.java
+++ src/main/java/com/edson/app/item/clientes/ProdutoClienteRest.java
@@ -3,18 +3,30 @@ package com.edson.app.item.clientes;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.edson.app.item.models.Produto;
 
 @FeignClient(name = "servico-produtos")
 public interface ProdutoClienteRest {
 	
-	@GetMapping("/produtos")
+	@GetMapping("/")
 	public List<Produto> findAll();
 	
-	@GetMapping("/produtos/{id}")
+	@GetMapping("/{id}")
 	public Produto findById(@PathVariable Long id);
 	
+	@PostMapping("/")
+	public Produto save(@RequestBody Produto produto);
+	
+	@PutMapping("/{id}")
+	public Produto update(@RequestBody Produto produto, @PathVariable Long id);
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id);
 }
